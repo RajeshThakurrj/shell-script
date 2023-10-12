1b755da8c83caf463202ba0628e0da25de11d4bf
Rajesh Kumar <rajeshkumar22293@gmail.com>
2023-10-12 05:18:17 +0000

    shell script of routetracking.sh

---
+++ routetracker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##########################
+#  Author:- Rajesh Kumar
+#  Date:- 12/oct/2023
+#  version:- v1
+#  This script will report the AWS Resource Usage
+#########################
+# Resources we are going to track are 
+#aws s3
+#aws ec2
+#aws lambda
+#aws iam users
+## list s3 buckets
+echo "print list of s3 bucket"
+aws s3 ls >>routetr
+
+## list ec2 instances
+echo "print list of ec2 instances"
+aws ec2 describe-instances >>routetr
+
+## list of aws lambda function
+echo "print list of lambda function"
+aws lambda list-functions >>routetr
+
+##list IAM users
+echo "print list of IAM users"
+aws iam list-users >>routetr
+
